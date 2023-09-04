322ce93b9e9e0117582602ec8e5d082c9b863218
Irakli Goderdzishvili <irakli.goder@gmail.com>
2023-09-04 06:45:13 +0400

    Adding Socreboard component. Will add Tailwind now.

--- src/components/timer/TimerMain.tsx
+++ src/components/timer/TimerMain.tsx
@@ -1,6 +1,33 @@
 import { useState } from 'react'
 import useCountdown from './useCountdown'
 
+const Scoreboard: React.FC = () => {
+	return (
+		<>
+			<div className="scoreboard">
+				<div className="top">
+					<div className="left"></div>
+					<div className="right">
+						<div className="score-number correct-guess-number">
+							<span className="score-number-value">49</span>
+							<span className="score-number-label">Correct</span>
+						</div>
+						<div className="score-number incorrect-guess-number">
+							<span className="score-number-value">3</span>
+							<span className="score-number-label">Incorrect</span>
+						</div>
+						<div className="score-number accuracy-number">
+							<span className="score-number-value">92%</span>
+							<span className="score-number-label">Accuracy</span>
+						</div>
+					</div>
+				</div>
+				<div className="bottom"></div>
+			</div>
+		</>
+	)
+}
+
 export default function TimerMain() {
 	// const seconds = 5
 	const [seconds, setSeconds] = useState(5)
