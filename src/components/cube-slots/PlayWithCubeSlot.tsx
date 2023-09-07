5a9b8db3db177a84a96b3a4b2ad6a57ca5deca69
Irakli Goderdzishvili <irakli.goder@gmail.com>
2023-09-07 10:26:03 +0300

    Confirmed: Slots work for not rendering cube again and again.

--- src/components/cube-slots/PlayWithCubeSlot.tsx
+++ src/components/cube-slots/PlayWithCubeSlot.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
+import Scoreboard from '../timer/Scoreboard'
+import useKeypress from '../new/useKeypress'
+import useGame from '../play-mode/useGame'
+import useCountdown from '../timer/useCountdown'
+import TimerControls from './TimerControls'
+import { CubeWithPos } from '../../ts/CubeClass3'
+import { CubeView } from './CubeView'
+import Cube from '../new/Cube'
+
+interface PlayPanelProps {
+	slot: React.ReactNode
+}
 
 interface IProps {}
 
-const Component: React.FC<IProps> = () => {
-	return <div></div>
+const PlayPanel: React.FC<PlayPanelProps> = ({ slot }) => {
+	const seconds = 30
+	const timer = useCountdown({
+		seconds,
+	})
+
+	return (
+		<>
+			<Scoreboard
+				correctGuesses={1}
+				incorrectGuesses={1}
+				millisecondsLeft={timer.millisecondsLeft}
+				secondsTotal={seconds}
+				debug
+			/>
+			{slot}
+			<TimerControls timer={timer} />
+		</>
+	)
 }
 
-export default Component
+const PlayWithCubeSlot: React.FC<IProps> = () => {
+	const cube = useMemo(() => new CubeWithPos(), [])
+	return <PlayPanel slot={<CubeView cube={cube} debug />}></PlayPanel>
+}
+
+export default PlayWithCubeSlot
