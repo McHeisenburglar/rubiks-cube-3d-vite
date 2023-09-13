2942dae4f8aeba8707dfd9a3cca9c73797ccb68a
Irakli Goderdzishvili <irakli.goder@gmail.com>
2023-09-14 00:13:40 +0200

    Add data-sticker-index property to Sticker, applying --sticker-corner-radius and --sticker-radius dynamically.

--- src/components/ui/customize/CustomizePanel.tsx
+++ src/components/ui/customize/CustomizePanel.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+import { CubeComponent } from '../../scene-refactor/Main'
+import { CubeWithPos } from '../../../ts/CubeClass3'
 
 interface IProps {}
 
+function useCube() {
+	const cube = useMemo(() => new CubeWithPos(), [])
+	return { cube }
+}
+
 const Component: React.FC<IProps> = () => {
-	return <div>HELLOOO</div>
+	const { cube } = useCube()
+
+	const style = {
+		'--sticker-radius': '12px',
+		'--sticker-corner-radius': '12px',
+	} as React.CSSProperties
+
+	return (
+		<div style={style}>
+			<CubeComponent cube={cube} debug />
+		</div>
+	)
 }
 
 export default Component
