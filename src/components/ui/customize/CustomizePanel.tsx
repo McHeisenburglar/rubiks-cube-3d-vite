c8529e424db5e8fbd461f2d2c4b27752d921059f
Irakli Goderdzishvili <irakli.goder@gmail.com>
2023-09-14 01:00:17 +0200

    Add useDocumentTitle hook for adding document tiles. Add rotation-duration CSS property. Calling it a night.

--- src/components/ui/customize/CustomizePanel.tsx
+++ src/components/ui/customize/CustomizePanel.tsx
@@ -1,6 +1,7 @@
 import React, { useMemo } from 'react'
 import { CubeComponent } from '../../scene-refactor/Main'
 import { CubeWithPos } from '../../../ts/CubeClass3'
+import { useDocumentTitle } from './useDocumentTitle'
 
 interface IProps {}
 
@@ -10,13 +11,15 @@ function useCube() {
 }
 
 const Component: React.FC<IProps> = () => {
+	useDocumentTitle('Customize Panel')
 	const { cube } = useCube()
 
 	const style = {
+		'--rotation-duration': '0.1s',
 		'--side-radius': '12px',
 		'--sticker-radius': '12px',
 		'--sticker-corner-radius': '12px',
-		'--sticker-padding': '8px',
+		'--sticker-padding': '4px',
 	} as React.CSSProperties
 
 	return (
