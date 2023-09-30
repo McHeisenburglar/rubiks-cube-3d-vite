48962460b4bb32bdd6e79a7018619b3be4eee044
Irakli Goderdzishvili <irakli.goder@gmail.com>
2023-09-30 20:58:05 +0200

    I love and hate this project. I just want to rewrite the whole thing.

--- src/components/sticker-cards/Main.tsx
+++ src/components/sticker-cards/Main.tsx
@@ -1,6 +1,5 @@
 import React, { useMemo } from 'react'
 import { CubeWithPos } from '../../ts/CubeClass3'
-import { CubeView } from '../cube-slots/CubeView'
 
 interface IProps {}
 
@@ -9,25 +8,79 @@ const StickerCard: React.FC<IProps> = () => {
 
 	const sticker = cube.getRandomStickerInFilter((s) => s.type === 'corner')
 
+	console.log('Showing sticker', sticker.name)
+
 	return (
 		<>
-			<CubeView cube={cube} debug />
-			<div className={` sticker-card ${sticker.type} `}>
-				<div
-					className={`sticker-card-face main-face`}
-					data-sticker-color={sticker.side}
-				></div>
-				{sticker.neighbors.map((s) => {
-					return (
-						<div
-							className={`sticker-card-face neighbor-face`}
-							data-sticker-color={s.side}
-						></div>
-					)
-				})}
-			</div>
+			<CubePieceView sticker={sticker} cubeConfig={cube.cubeConfig} debug />
 		</>
 	)
 }
 
 export default StickerCard
+
+import CubePerspectiveWrapper from '../new/CubePerspectiveWrapper'
+import CubeRotationController from '../new/CubeRotationWrapper'
+import CubeStyleProvider from '../new/CubeStyleConfigWrapper'
+
+interface CubeView {
+	sticker: ISticker
+	cubeConfig: CubeConfig
+	debug?: boolean
+	onStickerClick?: (sticker: ISticker) => void
+}
+
+const CubePieceView: React.FC<CubeView> = ({ debug, sticker, cubeConfig }) => {
+	if (debug) console.log('::::: Rendered CubeComponent.')
+
+	return (
+		<div className="cube-v2">
+			<div className="cube-wrapper">
+				<CubeRotationController>
+					<CubeStyleProvider config={cubeConfig}>
+						<CubePerspectiveWrapper mode={'3d-fold'}>
+							<CubePiece sticker={sticker} />
+						</CubePerspectiveWrapper>
+					</CubeStyleProvider>
+				</CubeRotationController>
+			</div>
+		</div>
+	)
+}
+
+import Sticker from '../new/Sticker'
+
+interface CubePieceProps {
+	sticker: ISticker
+	debug?: boolean
+}
+
+const CubePiece: React.FC<CubePieceProps> = ({ sticker, debug }) => {
+	if (debug) console.log('::::: Rendered Cube.')
+
+	return (
+		<div className={`cube`}>
+			<CubeSide sticker={sticker} side="top" />
+			{sticker.neighbors?.map((n, i) => {
+				return <CubeSide sticker={n} side={i === 0 ? 'front' : 'right'} />
+			})}
+		</div>
+	)
+}
+
+/* CUBE SIDE */
+interface CubeSideProps {
+	sticker: ISticker
+	debug?: boolean
+	side: Side
+}
+
+export const CubeSide: React.FC<CubeSideProps> = ({ sticker, debug, side }) => {
+	if (debug) console.log('::::: Rendered CubeSide.')
+
+	return (
+		<div className={`side piece-side side-${side}`} data-side={side}>
+			<Sticker sticker={sticker} index={0} position={sticker.side + 0} />
+		</div>
+	)
+}
