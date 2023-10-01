258e2f1ee170229060131d77896f8a0690c8a350
Irakli Goderdzishvili <irakli.goder@gmail.com>
2023-10-01 02:41:00 +0200

    Mostly just playing with sexy animations.

--- src/components/input/InputPage.tsx
+++ src/components/input/InputPage.tsx
@@ -54,11 +54,11 @@ interface IProps {
   options: Array<string | number>;
 }
 
-interface SwitchListProps {
-    options: 
-}
+// interface SwitchListProps {
+//     options:
+// }
 
-export const SwitchList: React.FC<SwitchListProps>
+// export const SwitchList: React.FC<SwitchListProps>;
 
 export const SwitchRow: React.FC<IProps> = ({ label, options }) => {
   const [selectedOption, setSelectedOption] = useState<number | null>(null);
@@ -67,9 +67,9 @@ export const SwitchRow: React.FC<IProps> = ({ label, options }) => {
     setSelectedOption(index);
   };
   return (
-    <div className="bg-white max-w-xl px-4 py-2 m-auto flex justify-between items-center">
-      <h3 className="wide text-xl">{label}</h3>
-      <div className="options flex gap-2 flex-wrap">
+    <div className="bg-white max-w-xl px-4 py-8 m-auto flex justify-center items-center">
+      {/* <h3 className="wide text-xl">{label}</h3> */}
+      <div className="options flex gap-2 justify-center flex-wrap">
         {options.map((option, index) => {
           return (
             <SwitchButton
@@ -101,9 +101,9 @@ const Main = () => {
 
   return (
     <>
-      {optionSets.map((set, index) => (
+      {/* {optionSets.map((set, index) => (
         <SwitchRow label={set.label} key={index} options={set.options} />
-      ))}
+      ))} */}
       <SwitchRow
         label={"Options"}
         options={[
