d20bae1e8862a1d7357eb0c61717dd3121a197cd
Irakli Goderdzishvili <irakli.goder@gmail.com>
2023-10-02 16:13:52 +0200

    Add pieceType option to useGame

--- src/components/play-mode/Main.tsx
+++ src/components/play-mode/Main.tsx
@@ -1,254 +1,263 @@
-import React, { useEffect, useMemo, useState } from 'react'
-import useCountdown from '../timer/useCountdown'
-import Scoreboard from '../timer/Scoreboard'
+import React, { useEffect, useMemo, useState } from "react";
+import useCountdown from "../timer/useCountdown";
+import Scoreboard from "../timer/Scoreboard";
 
-import { CubeComponent } from '../scene-refactor/Main'
-import { CubeWithPos } from '../../ts/CubeClass3'
-import useKeypress from '../new/useKeypress'
-import useGame from './useGame'
+import { CubeComponent } from "../scene-refactor/Main";
+import { CubeWithPos } from "../../ts/CubeClass3";
+import useKeypress from "../new/useKeypress";
+import useGame from "./useGame";
 
 interface PlayModeControlsProps {
-	debug?: boolean
-	isRunning: boolean
-	isPaused: boolean
-	onClickStart: () => void
-	onClickPause: () => void
-	onClickStop: () => void
+    debug?: boolean;
+    isRunning: boolean;
+    isPaused: boolean;
+    onClickStart: () => void;
+    onClickPause: () => void;
+    onClickStop: () => void;
 }
 
 const PlayModeControls: React.FC<PlayModeControlsProps> = (props) => {
-	if (props.debug) console.log('::::: Rendered PlayModeControls.')
+    if (props.debug) console.log("::::: Rendered PlayModeControls.");
 
-	const { isRunning, isPaused, onClickStart, onClickPause, onClickStop } = props
+    const { isRunning, isPaused, onClickStart, onClickPause, onClickStop } =
+        props;
 
-	if (!isRunning) {
-		return (
-			<div className="text-center">
-				<button className="btn-primary success" onClick={onClickStart}>
-					Start game
-				</button>
-			</div>
-		)
-	} else {
-		return (
-			<div className="text-center">
-				<button className="btn" onClick={onClickPause}>
-					{isPaused ? 'Unpause' : 'Pause'}
-				</button>
-				<button className="btn" onClick={onClickStop}>
-					Stop game
-				</button>
-			</div>
-		)
-	}
-}
+    if (!isRunning) {
+        return (
+            <div className="text-center">
+                <button className="btn-primary success" onClick={onClickStart}>
+                    Start game
+                </button>
+            </div>
+        );
+    } else {
+        return (
+            <div className="text-center">
+                <button className="btn" onClick={onClickPause}>
+                    {isPaused ? "Unpause" : "Pause"}
+                </button>
+                <button className="btn" onClick={onClickStop}>
+                    Stop game
+                </button>
+            </div>
+        );
+    }
+};
 
 const GameComponentDev: React.FC<{ game: ReturnType<typeof useGame> }> = ({
-	game,
+    game,
 }) => {
-	return (
-		<>
-			{game.inProgress && (
-				<div className="text-left">
-					<h1 className="text-lg font-bold mb-2">Game component</h1>
-					<ul>
-						<li>Current letter to guess: {game.currentSticker?.name}</li>
-						<li>Correct: {game.correct}</li>
-						<li>Incorrect: {game.incorrect}</li>
-					</ul>
-				</div>
-			)}
-		</>
-	)
-}
+    return (
+        <>
+            {game.inProgress && (
+                <div className="text-left">
+                    <h1 className="text-lg font-bold mb-2">Game component</h1>
+                    <ul>
+                        <li>
+                            Current letter to guess: {game.currentSticker?.name}
+                        </li>
+                        <li>Correct: {game.correct}</li>
+                        <li>Incorrect: {game.incorrect}</li>
+                    </ul>
+                </div>
+            )}
+        </>
+    );
+};
 
-import RotationContextWrapper from '../scene-refactor/RotationContextWrapper'
-import HighlightContextWrapper from '../scene-refactor/HighlightContextProvider'
-import { useSpotlightContext } from '../scene-refactor/useSpotlight'
-import { CorrectGuessLog, GuessLogEntry } from '../timer/TimerMain'
-import { useDocumentTitle } from '../ui/customize/useDocumentTitle'
+import RotationContextWrapper from "../scene-refactor/RotationContextWrapper";
+import HighlightContextWrapper from "../scene-refactor/HighlightContextProvider";
+import { useSpotlightContext } from "../scene-refactor/useSpotlight";
+import { CorrectGuessLog, GuessLogEntry } from "../timer/TimerMain";
+import { useDocumentTitle } from "../ui/customize/useDocumentTitle";
 
 const SeparateTimerComponent = () => {
-	const gameTimer = useCountdown({
-		seconds: 10,
-		onStart: () => {},
-		onStop: () => {},
-	})
-	useEffect(() => {
-		gameTimer.start()
-	}, [])
+    const gameTimer = useCountdown({
+        seconds: 10,
+        onStart: () => {},
+        onStop: () => {},
+    });
+    useEffect(() => {
+        gameTimer.start();
+    }, []);
 
-	return (
-		<>
-			<div>{gameTimer.millisecondsLeft}</div>
-		</>
-	)
-}
+    return (
+        <>
+            <div>{gameTimer.millisecondsLeft}</div>
+        </>
+    );
+};
 
 const PlayModeComponent = () => {
-	const cube = useMemo<CubeWithPos>(() => {
-		const cube = new CubeWithPos()
-		cube.scramble()
-		return cube
-	}, [])
+    const cube = useMemo<CubeWithPos>(() => {
+        const cube = new CubeWithPos();
+        cube.scramble();
+        return cube;
+    }, []);
 
-	return (
-		<>
-			<SeparateTimerComponent />
-			<HighlightContextWrapper>
-				<RotationContextWrapper>
-					<CubeComponent cube={cube} />
-				</RotationContextWrapper>
-			</HighlightContextWrapper>
-		</>
-	)
-}
+    return (
+        <>
+            <SeparateTimerComponent />
+            <HighlightContextWrapper>
+                <RotationContextWrapper>
+                    <CubeComponent cube={cube} />
+                </RotationContextWrapper>
+            </HighlightContextWrapper>
+        </>
+    );
+};
 
 const PlayModeComponent2: React.FC = () => {
-	console.log('rendered')
-	const [scramble, setScramble] = useState<string | null>(null)
-	const cube: CubeWithPos = useMemo<CubeWithPos>(() => {
-		const cube = new CubeWithPos()
-		if (scramble) cube.performAlgorithm(scramble)
-		return cube
-	}, [scramble])
+    console.log("rendered");
+    const [scramble, setScramble] = useState<string | null>(null);
+    const cube: CubeWithPos = useMemo<CubeWithPos>(() => {
+        const cube = new CubeWithPos();
+        if (scramble) cube.performAlgorithm(scramble);
+        return cube;
+    }, [scramble]);
 
-	const { setSpotlight, clearSpotlight } = useSpotlightContext()
+    const { setSpotlight, clearSpotlight } = useSpotlightContext();
 
-	const game = useGame({
-		cube,
-		onStickerChange: (sticker) => {
-			setSpotlight(sticker)
-		},
-		onCorrectGuess: (sticker) => {
-			logCorrectGuess(sticker)
-		},
-		onIncorrectGuess: (sticker) => {
-			const domElement = document.querySelector(
-				`[data-sticker-id=${sticker.id}]`
-			)
-			if (domElement) pulseAnimation(domElement)
-		},
-		onGameStop: () => {
-			clearSpotlight()
-		},
-	})
+    const game = useGame({
+        cube,
+        pieceType: "edge",
+        onStickerChange: (sticker) => {
+            setSpotlight(sticker);
+        },
+        onCorrectGuess: (sticker) => {
+            logCorrectGuess(sticker);
+        },
+        onIncorrectGuess: (sticker) => {
+            const domElement = document.querySelector(
+                `[data-sticker-id=${sticker.id}]`
+            );
+            if (domElement) pulseAnimation(domElement);
+        },
+        onGameStop: () => {
+            clearSpotlight();
+        },
+    });
 
-	const handleScramble = () => {
-		const scramble = cube.scramble()
-		console.log('got here')
-		setScramble(scramble)
-	}
+    const handleScramble = () => {
+        const scramble = cube.scramble();
+        console.log("got here");
+        setScramble(scramble);
+    };
 
-	const pulseAnimation = (domElement: Element) => {
-		domElement.classList.add('error')
-		setTimeout(() => {
-			domElement.classList.remove('error')
-		}, 300)
-	}
+    const pulseAnimation = (domElement: Element) => {
+        domElement.classList.add("error");
+        setTimeout(() => {
+            domElement.classList.remove("error");
+        }, 300);
+    };
 
-	useKeypress((e) => {
-		e.preventDefault()
-		if (!game.inProgress) {
-			const sticker = cube.getStickerByLetter(e.key, 'edge')
-			if (sticker) setSpotlight(sticker)
-			return
-		}
-		if (gameTimer.isRunning && !gameTimer.isPaused && game.inProgress) {
-			game.checkGuess(e)
-		}
-	})
+    useKeypress((e) => {
+        e.preventDefault();
+        if (!game.inProgress) {
+            const sticker = cube.getStickerByLetter(e.key, "edge");
+            if (sticker) setSpotlight(sticker);
+            return;
+        }
+        if (gameTimer.isRunning && !gameTimer.isPaused && game.inProgress) {
+            game.checkGuess(e);
+        }
+    });
 
-	const gameOptions = {
-		seconds: 10,
-		type: 'corner',
-	}
+    const gameOptions = {
+        seconds: 10,
+        type: "corner",
+    };
 
-	const gameTimer = useCountdown({
-		seconds: gameOptions.seconds,
-		onStart: () => {
-			game.start()
-		},
-		onStop: () => {
-			game.stop()
-		},
-		onUnpause: () => {
-			if (game.currentSticker) setSpotlight(game.currentSticker)
-		},
-		onCompletion() {
-			game.stop()
-			clearSpotlight()
-		},
-	})
+    const gameTimer = useCountdown({
+        seconds: gameOptions.seconds,
+        onStart: () => {
+            game.start();
+        },
+        onStop: () => {
+            game.stop();
+        },
+        onUnpause: () => {
+            if (game.currentSticker) setSpotlight(game.currentSticker);
+        },
+        onCompletion() {
+            game.stop();
+            clearSpotlight();
+        },
+    });
 
-	const logCorrectGuess = (sticker: ISticker) => {
-		const ms = gameTimer.newMarker()
-		const logEntry: GuessLogEntry = {
-			no: game.guessLog.length + 1,
-			sticker: sticker.name,
-			time: ms,
-		}
-		game.addGuessLogEntry(logEntry)
-	}
+    const logCorrectGuess = (sticker: ISticker) => {
+        const ms = gameTimer.newMarker();
+        const logEntry: GuessLogEntry = {
+            no: game.guessLog.length + 1,
+            sticker: sticker.name,
+            time: ms,
+        };
+        game.addGuessLogEntry(logEntry);
+    };
 
-	const handleStickerClick = (sticker: ISticker) => {
-		if (!game.inProgress) setSpotlight(sticker)
-	}
+    const handleStickerClick = (sticker: ISticker) => {
+        if (!game.inProgress) setSpotlight(sticker);
+    };
 
-	return (
-		<div className="mx-auto max-w-[60rem] px-24 bg-white pb-8">
-			<Scoreboard
-				correctGuesses={game.correct}
-				incorrectGuesses={game.incorrect}
-				secondsTotal={gameOptions.seconds}
-				millisecondsLeft={gameTimer.millisecondsLeft}
-			/>
-			<div className="text-center">
-				<CubeComponent cube={cube} onStickerClick={handleStickerClick} />
-			</div>
-			{!game.inProgress && (
-				<>
-					{scramble && (
-						<div className="text-center relative -top-8">
-							<span className="text-xs">Scramble: {scramble}</span>
-						</div>
-					)}
-					<div className="text-center">
-						<button
-							className="btn-primary relative mb-4"
-							onClick={handleScramble}
-						>
-							Scramble cube
-						</button>
-					</div>
-				</>
-			)}
-			<PlayModeControls
-				isPaused={gameTimer.isPaused}
-				isRunning={gameTimer.isRunning}
-				onClickStart={gameTimer.start}
-				onClickPause={gameTimer.togglePause}
-				onClickStop={gameTimer.stop}
-			/>
-			<div className="mt-16">
-				<GameComponentDev game={game} />
-			</div>
-			{!game.inProgress && game.guessLog.length > 0 && (
-				<CorrectGuessLog log={game.guessLog} />
-			)}
-		</div>
-	)
-}
+    return (
+        <div className="mx-auto max-w-[60rem] px-24 bg-white pb-8">
+            <Scoreboard
+                correctGuesses={game.correct}
+                incorrectGuesses={game.incorrect}
+                secondsTotal={gameOptions.seconds}
+                millisecondsLeft={gameTimer.millisecondsLeft}
+            />
+            <div className="text-center">
+                <CubeComponent
+                    cube={cube}
+                    onStickerClick={handleStickerClick}
+                />
+            </div>
+            {!game.inProgress && (
+                <>
+                    {scramble && (
+                        <div className="text-center relative -top-8">
+                            <span className="text-xs">
+                                Scramble: {scramble}
+                            </span>
+                        </div>
+                    )}
+                    <div className="text-center">
+                        <button
+                            className="btn-primary relative mb-4"
+                            onClick={handleScramble}
+                        >
+                            Scramble cube
+                        </button>
+                    </div>
+                </>
+            )}
+            <PlayModeControls
+                isPaused={gameTimer.isPaused}
+                isRunning={gameTimer.isRunning}
+                onClickStart={gameTimer.start}
+                onClickPause={gameTimer.togglePause}
+                onClickStop={gameTimer.stop}
+            />
+            <div className="mt-16">
+                <GameComponentDev game={game} />
+            </div>
+            {!game.inProgress && game.guessLog.length > 0 && (
+                <CorrectGuessLog log={game.guessLog} />
+            )}
+        </div>
+    );
+};
 
 const PlayModeExport = () => {
-	useDocumentTitle('Play Mode')
-	return (
-		<HighlightContextWrapper>
-			<RotationContextWrapper>
-				<PlayModeComponent2 />
-			</RotationContextWrapper>
-		</HighlightContextWrapper>
-	)
-}
+    useDocumentTitle("Play Mode");
+    return (
+        <HighlightContextWrapper>
+            <RotationContextWrapper>
+                <PlayModeComponent2 />
+            </RotationContextWrapper>
+        </HighlightContextWrapper>
+    );
+};
 
-export default PlayModeExport
+export default PlayModeExport;
--- src/components/play-mode/useGame.ts
+++ src/components/play-mode/useGame.ts
@@ -1,108 +1,109 @@
-import { useEffect, useState } from 'react'
-import { CubeWithPos } from '../../ts/CubeClass3'
-import { GuessLogEntry } from '../timer/TimerMain'
+import { useEffect, useState } from "react";
+import { CubeWithPos } from "../../ts/CubeClass3";
+import { GuessLogEntry } from "../timer/TimerMain";
 
 interface useGameOptions {
-	cube: CubeWithPos
-	onStickerChange?: (sticker: ISticker) => void
-	onCorrectGuess?: (sticker: ISticker) => void
-	onIncorrectGuess?: (sticker: ISticker) => void
-	onGameStart?: () => void
-	onGameStop?: () => void
+    cube: CubeWithPos;
+    pieceType: PieceType;
+    onStickerChange?: (sticker: ISticker) => void;
+    onCorrectGuess?: (sticker: ISticker) => void;
+    onIncorrectGuess?: (sticker: ISticker) => void;
+    onGameStart?: () => void;
+    onGameStop?: () => void;
 }
 
 export const useGame = (options: useGameOptions) => {
-	const { cube } = options
+    const { cube, pieceType } = options;
 
-	const [inProgress, setInProgress] = useState(false)
-	const [correct, setCorrect] = useState(0)
-	const [incorrect, setIncorrect] = useState(0)
+    const [inProgress, setInProgress] = useState(false);
+    const [correct, setCorrect] = useState(0);
+    const [incorrect, setIncorrect] = useState(0);
 
-	const [guessLog, setGuessLog] = useState<GuessLogEntry[]>([])
+    const [guessLog, setGuessLog] = useState<GuessLogEntry[]>([]);
 
-	const [currentSticker, setCurrentSticker] = useState<ISticker | null>(null)
+    const [currentSticker, setCurrentSticker] = useState<ISticker | null>(null);
 
-	const nextRandomSticker = () => {
-		const newSticker = cube.getRandomStickerInFilter(
-			(sticker) =>
-				sticker.type === 'corner' && sticker.id !== currentSticker?.id
-		)
-		setCurrentSticker(newSticker)
-	}
+    const nextRandomSticker = () => {
+        const newSticker = cube.getRandomStickerInFilter(
+            (sticker) =>
+                sticker.type === pieceType && sticker.id !== currentSticker?.id
+        );
+        setCurrentSticker(newSticker);
+    };
 
-	const reset = () => {
-		setCurrentSticker(null)
-		setCorrect(0)
-		setIncorrect(0)
-		setGuessLog([])
-	}
+    const reset = () => {
+        setCurrentSticker(null);
+        setCorrect(0);
+        setIncorrect(0);
+        setGuessLog([]);
+    };
 
-	useEffect(() => {
-		if (currentSticker) {
-			options.onStickerChange?.(currentSticker)
-		}
-	}, [currentSticker])
+    useEffect(() => {
+        if (currentSticker) {
+            options.onStickerChange?.(currentSticker);
+        }
+    }, [currentSticker]);
 
-	const checkGuess = (e: KeyboardEvent) => {
-		const guessedSticker = cube.getStickerByLetter(
-			e.key.toLowerCase(),
-			'corner'
-		)
-		if (!guessedSticker || !currentSticker) return
+    const checkGuess = (e: KeyboardEvent) => {
+        const guessedSticker = cube.getStickerByLetter(
+            e.key.toLowerCase(),
+            pieceType
+        );
+        if (!guessedSticker || !currentSticker) return;
 
-		if (guessedSticker.id === currentSticker.id) {
-			options.onCorrectGuess?.(currentSticker)
-			onCorrectGuess()
-		} else {
-			options.onIncorrectGuess?.(guessedSticker)
-			onIncorrectGuess()
-		}
-	}
+        if (guessedSticker.id === currentSticker.id) {
+            options.onCorrectGuess?.(currentSticker);
+            onCorrectGuess();
+        } else {
+            options.onIncorrectGuess?.(guessedSticker);
+            onIncorrectGuess();
+        }
+    };
 
-	const onCorrectGuess = () => {
-		setCorrect(correct + 1)
-		nextRandomSticker()
-	}
+    const onCorrectGuess = () => {
+        setCorrect(correct + 1);
+        nextRandomSticker();
+    };
 
-	const onIncorrectGuess = () => {
-		setIncorrect(incorrect + 1)
-	}
+    const onIncorrectGuess = () => {
+        setIncorrect(incorrect + 1);
+    };
 
-	const start = () => {
-		setInProgress(true)
-		reset()
-		nextRandomSticker()
+    const start = () => {
+        setInProgress(true);
+        reset();
+        nextRandomSticker();
 
-		options.onGameStart?.()
-	}
+        options.onGameStart?.();
+    };
 
-	const stop = () => {
-		setInProgress(false)
+    const stop = () => {
+        setInProgress(false);
 
-		options.onGameStop?.()
-	}
+        options.onGameStop?.();
+    };
 
-	const skip = () => {
-		nextRandomSticker()
-	}
+    const skip = () => {
+        nextRandomSticker();
+    };
 
-	const addGuessLogEntry = (entry: GuessLogEntry) => {
-		setGuessLog((prev) => [...prev, entry])
-	}
+    const addGuessLogEntry = (entry: GuessLogEntry) => {
+        setGuessLog((prev) => [...prev, entry]);
+    };
 
-	return {
-		inProgress,
-		start,
-		stop,
-		currentSticker,
-		checkGuess,
-		reset,
-		correct,
-		incorrect,
-		skip,
-		guessLog,
-		addGuessLogEntry,
-	}
-}
+    return {
+        inProgress,
+        start,
+        stop,
+        currentSticker,
+        checkGuess,
+        reset,
+        correct,
+        incorrect,
+        skip,
+        guessLog,
+        addGuessLogEntry,
+    };
+};
 
-export default useGame
+export default useGame;
