5ee7bd01b4708dc71cb5c7df681d7f8d0edf079b
Irakli Goderdzishvili <irakli.goder@gmail.com>
2023-10-03 06:10:33 +0200

    Tooltips hell yeah?

--- src/components/input/InputPage.tsx
+++ src/components/input/InputPage.tsx
@@ -108,9 +108,70 @@ const Main = () => {
                         />
                     </ul>
                 </div>
+                <div className="mt-8">
+                    <TooltipWrapper>
+                        <FontAwesomeIcon
+                            icon={faStopwatch}
+                            size="lg"
+                            className="-mt-[2px] text-slate-500 duration-200 hover:text-slate-600"
+                        />
+                    </TooltipWrapper>
+                </div>
             </div>
         </>
     );
+};
+
+interface TooltipProps {
+    content: string;
+    children: ChildElement;
+}
+
+const TooltipWrapper: React.FC<TooltipProps> = ({ children }) => {
+    const [showTooltip, setShowTooltip] = useState(false);
+    const [showTooltipClass, setShowTooltipClass] = useState(false);
+    const bgColor = "bg-slate-800";
+
+    const onMouseEnter = () => {
+        setShowTooltip(true);
+        setTimeout(() => {
+            setShowTooltipClass(true);
+        }, 10);
+    };
+
+    const onMouseLeave = () => {
+        setShowTooltipClass(false);
+        setTimeout(() => {
+            if (!showTooltipClass) setShowTooltip(false);
+        }, 200);
+    };
+
+    return (
+        <div
+            className="relative inline-block"
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+        >
+            <FontAwesomeIcon icon={faStopwatch} />
+            {showTooltip && (
+                <span
+                    className={`absolute left-1/2 top-0 inline-flex max-w-xl -translate-x-1/2 -translate-y-full flex-col items-center text-center ${
+                        showTooltipClass
+                            ? "opacity-1 z-10"
+                            : "pointer-events-none invisible translate-y-[-90%] opacity-0"
+                    } duration-300`}
+                >
+                    <span
+                        className={`inline-block rounded-md ${bgColor} text-medium w-max max-w-xs px-3 py-2 text-xs text-white`}
+                    >
+                        Seconds in game
+                    </span>
+                    <span className="-m-[1px] w-[16px] border-l-[8px] border-r-[8px] border-t-[8px] border-l-transparent border-r-transparent border-t-slate-800"></span>
+                </span>
+            )}
+        </div>
+    );
+    // border-l-[50px] border-l-transparent
 };
 
 export default Main;
